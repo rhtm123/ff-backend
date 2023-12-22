86f5b8e59787c87db5278b90176aebda85971ad0
rhtm123 <maurya.iitk@gmail.com>
2023-12-22 20:19:57 +0530

    .env file added

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 // db.js
 const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables from .env file
+
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://mongo:-fGEG4-FGEDdaC445-gCEAgehA2HhabB@monorail.proxy.rlwy.net:15855', { });
+    await mongoose.connect(process.env.MONGODB_URI, { });
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
